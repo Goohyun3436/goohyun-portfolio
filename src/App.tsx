a00c58acf825e9fbf14184cfdc4a2668d38523e6
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-12-22 03:20:12 +0900

    Add: 맥 터미널 창 레이아웃 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
+import Main from './pages/Main';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </>
   );
 };
