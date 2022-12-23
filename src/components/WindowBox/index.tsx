29d8b012b269134cecc8d2e98c8bd7216e409b72
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2022-12-23 23:33:46 +0900

    Add: 맥 터미널 창 상태바에 명령어 입력시 경로이동 기능 추가

--- src/components/WindowBox/index.tsx
+++ src/components/WindowBox/index.tsx
@@ -8,16 +8,11 @@ const WindowBox = () => {
     <WindowBoxBlock className="window-box-block">
       <TopBar />
       <div className="content">
-        <p>다음은 여러가지 상황에서 자주 사용하는 git 명령입니다.</p>
-        <br />
-        <br />
-        <p>작업 공간 시작 (참고: git help history)</p>
+        <p>작업 공간 시작 명령어 (참고: git help history)</p>
         <br />
         <Command command="overview" description="해당 프로젝트의 컨셉을 설명합니다." />
         <Command command="portfolio" description="프로젝트 작업물을 소개합니다." />
         <Command command="contect" description="email, github 등 개인정보를 가져옵니다." />
-        <br />
-        <p style={{ color: 'rgb(145, 180, 245)' }}>' cd [명령어] ' 를 입력하세요.</p>
         <br />
         <StatusBar />
       </div>
@@ -33,7 +28,7 @@ const WindowBoxBlock = styled.div`
   background: rgb(40, 50, 56);
 
   .content {
-    padding: 1rem 0.5rem;
+    padding: 1rem;
     color: rgb(183, 188, 198);
     font-size: 1.2rem;
   }
